96f0122e20198e14671c2bf32db9dc89bf4cd746
Jong1co <narira0922@gmail.com>
2025-01-12 10:51:01 +0900

    feat: 시간에 따라 변하도록 만들어보기

--- pointillism/pointillism.js
+++ pointillism/pointillism.js
@@ -1,65 +1,81 @@
 class Pointillism {
   ctx;
+  canvasWidth;
+  canvasHeight;
 
-  constructor() {
+  constructor(url = "./bg2.jpeg") {
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext("2d");
     this.ctx = ctx;
 
     // 내가 원하는 최종 캔버스 크기(800×400)
-    const canvasWidth = 800;
-    const canvasHeight = 400;
+    this.canvasWidth = 800;
+    this.canvasHeight = 400;
 
-    canvas.style.width = canvasWidth + "px";
-    canvas.style.height = canvasHeight + "px";
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
+    canvas.style.width = this.canvasWidth + "px";
+    canvas.style.height = this.canvasHeight + "px";
+    canvas.width = this.canvasWidth;
+    canvas.height = this.canvasHeight;
 
+    this.createPointillismByUrl(ctx, url);
+  }
+
+  createPointillismByUrl(ctx, url) {
     const background = new Image();
-    background.src = "./bg.png";
+    background.src = url;
 
     // 이미지 로드 후 처리
     background.onload = () => {
       // cover 함수를 이용해 800×400 크기에 맞춰 그림
-      drawCoverImage(ctx, background, canvasWidth, canvasHeight);
+      drawCoverImage(ctx, background, this.canvasWidth, this.canvasHeight);
 
-      const pixelWidth = 8;
+      let i = 0;
 
-      for (let i = 0; i < canvasWidth / pixelWidth; i++) {
-        for (let j = 0; j < canvasHeight / pixelWidth; j++) {
-          console.log(i, j);
-          const startX = i * pixelWidth;
-          const startY = j * pixelWidth;
-          const endX = startX + pixelWidth;
-          const endY = startY + pixelWidth;
+      this.draw(i);
+      // const interval = setInterval(() => {
+      //   i++;
 
-          const centerX = startX + pixelWidth / 2;
-          const centerY = startY + pixelWidth / 2;
-
-          const color = this.findPixelInfo(ctx, centerX, centerY);
-
-          ctx.fillStyle = "black";
-          ctx.fillRect(startX, startY, pixelWidth, pixelWidth);
-
-          ctx.beginPath();
-          ctx.arc(centerX, centerY, pixelWidth / 2, 0, Math.PI * 2);
-          ctx.fillStyle = color;
-          ctx.fill();
-        }
-      }
+      //   if (i > 300) {
+      //     clearInterval(interval);
+      //   }
+      // }, 16);
     };
+  }
+
+  draw(column) {
+    const pixelWidth = 10;
+
+    for (let i = 0; i < this.canvasWidth / pixelWidth; i++) {
+      for (let j = 0; j < this.canvasHeight / pixelWidth; j++) {
+        const startX = i * pixelWidth;
+        const startY = j * pixelWidth;
+
+        const centerX = startX + pixelWidth / 2;
+        const centerY = startY + pixelWidth / 2;
+
+        const color = this.findPixelInfo(this.ctx, centerX, centerY);
+
+        this.ctx.fillStyle = "black";
+        this.ctx.fillRect(startX, startY, pixelWidth, pixelWidth);
+
+        this.ctx.beginPath();
+        this.ctx.arc(centerX, centerY, pixelWidth / 2 - 0.5, 0, Math.PI * 2);
+        this.ctx.fillStyle = color;
+        this.ctx.fill();
+      }
+    }
   }
 
   findPixelInfo(ctx, x, y) {
     // (x, y) 좌표의 1픽셀 정보를 가져온다.
     const imageData = ctx.getImageData(x, y, 1, 1);
-    const pixel = imageData.data;
 
     // pixel 배열에는 RGBA가 순서대로 들어있다.
     // pixel[0] -> Red
     // pixel[1] -> Green
     // pixel[2] -> Blue
     // pixel[3] -> Alpha (0 ~ 255)
+    const pixel = imageData.data;
 
     return `rgba(${pixel[0]}, ${pixel[1]}, ${pixel[2]}, ${pixel[3] / 255})`;
   }
@@ -97,4 +113,8 @@ function drawCoverImage(ctx, image, canvasWidth, canvasHeight) {
   );
 }
 
-new Pointillism();
+window.onload = function () {
+  new Pointillism();
+
+  // window.requestAnimationFrame();
+};
